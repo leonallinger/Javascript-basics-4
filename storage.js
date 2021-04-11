7082a3db12142dddab9a13df3ee688902d614475
leonallinger <leon.allinger@gmail.com>
2021-04-11 21:21:52 +0200

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.login_button');
+button.addEventListener('click', login);
+
+function login() {
+    email = document.getElementById("email").value;
+    password = document.getElementById("pwd").value;
+
+
+}
